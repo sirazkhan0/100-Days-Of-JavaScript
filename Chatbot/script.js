2430c868ea91062e56a2fe89b7a531a0ba73f3dc
siraz <siraz@gmail.com>
2023-06-17 12:11:36 +0530

    first

--- Chatbot/script.js
+++ Chatbot/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "e7926859f9mshab1608d846ec2d2p110b20jsnfdfbda091c26"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
@@ -19,7 +19,7 @@ const createChatLi = (message, className) => {
 }
 
 const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const API_URL = "https://openai80.p.rapidapi.com/models";
     const messageElement = chatElement.querySelector("p");
 
     // Define the properties and message for the API request
@@ -27,11 +27,12 @@ const generateResponse = (chatElement) => {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
+            "Authorization": `Bearer ${API_KEY}`,
+            "X-RapidAPI-Host": "openai80.p.rapidapi.com"
         },
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
+            messages: [{ role: "user", content: userMessage }],
         })
     }
 
@@ -46,7 +47,7 @@ const generateResponse = (chatElement) => {
 
 const handleChat = () => {
     userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
+    if (!userMessage) return;
 
     // Clear the input textarea and set its height to default
     chatInput.value = "";
@@ -55,7 +56,7 @@ const handleChat = () => {
     // Append the user's message to the chatbox
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
-    
+
     setTimeout(() => {
         // Display "Thinking..." message while waiting for the response
         const incomingChatLi = createChatLi("Thinking...", "incoming");
@@ -74,7 +75,7 @@ chatInput.addEventListener("input", () => {
 chatInput.addEventListener("keydown", (e) => {
     // If Enter key is pressed without Shift key and the window 
     // width is greater than 800px, handle the chat
-    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
         e.preventDefault();
         handleChat();
     }
